8c955c07866b8a66f99811dde8e2e355b3f900b1
janalam <janalam1224@gmail.com>
2025-05-07 21:56:37 +0500

    app.js is Added as Server File

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const authMiddleware = require('./middlewares/authMiddleware');
+const userRouter = require('./routes/userRouter');
+const authRouter = require('./routes/authRouter');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/auth', authRouter);
+app.use('/api', userRouter);  
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
