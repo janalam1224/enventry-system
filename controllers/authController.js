abb8d9519b9d9b3e3142512c3352c37671248918
janalam <janalam1224@gmail.com>
2025-05-08 16:21:16 +0500

    loginIndex is Added as Welcome page in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,31 @@
-exports.getLogin = (req, res, next) => {
-res.status(200).json({message: "Login page"});
-}
+const authLogin = [
+  { 
+    email: "janalam123@gmail.com",
+    password: "12345"
+  },
+  {
+    email: "kamran123@gmail.com",
+    password: "123"
+  }
+];
+
+exports.getIndex = (req, res) => {
+  res.send(`
+    <h1>Welcome to Home</h1>
+  `);
+};
 
 exports.postLogin = (req, res, next) => {
-  res.status(201).json({message: "login Successfully"});
-}
+  const { email, password } = req.body;
+
+  const validUser = authLogin.find(user => user.email === email && user.password === password);
+
+  if (validUser) {
+    return res.status(200).json({
+      message: "Login Successful",
+      token: "mysecrettoken"
+    });
+  } else {
+    return res.status(401).json({ message: "Invalid Credentials" });
+  }
+};
