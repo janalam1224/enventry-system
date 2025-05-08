bb644fff96220f0b40ea8ba21e3108a24fb33ab0
janalam <janalam1224@gmail.com>
2025-05-08 16:25:03 +0500

    in authRouter after Login redirect to welcome page

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,10 +3,9 @@ const authRouter = express.Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-authRouter.get('/login', authController.getLogin);
+authRouter.get("/", authController.getIndex);
 authRouter.post('/login', authController.postLogin);
 authRouter.get('/', authMiddleware, (req, res) => {
   res.json("welcome to Users Page");
 });
-
 module.exports = authRouter;
